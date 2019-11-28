c984aec958414f5ff77cd21269acfa964fa64422
Onur Aslan <onuraslan@gmail.com>
2019-11-28 19:58:09 +0300

    Only warn about cast_lossless and allow non_ascii_literal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![deny(clippy::all)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::cast_possible_truncation)]
+#![warn(clippy::cast_lossless)]
 #![allow(clippy::unreadable_literal)]
+#![allow(clippy::non_ascii_literal)]
 
 mod display;
 mod emulator;
